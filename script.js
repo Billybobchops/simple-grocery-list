f0b89a450a4b11caf34f6c13fa9fa2276105ee8a
Billybobchops <chlanda52@gmail.com>
2021-06-23 15:21:06 -0500

    figured out opacity problem

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function changeOpacity() {
+  document.querySelectorAll('.todo-item').forEach((todo) => {
+    if (todo.classList.contains('checked')) {
+      todo.style.opacity = '.3';
+    }
+  })
+}
+changeOpacity();
+
+function complete() {
+  const markComplete = function (e) {
+    console.log(e);
+  }
+  document.querySelector('.todo-items').addEventListener('click', markComplete);
+}
+complete();
