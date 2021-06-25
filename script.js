64595c146c2383def123b86376a8654b30c1ff66
Billybobchops <chlanda52@gmail.com>
2021-06-25 15:01:51 -0500

    beginning work on local storage and delete functionality

--- script.js
+++ script.js
@@ -1,68 +1,79 @@
 'use strict';
 
 const todosContainer = document.querySelector('.todos-container');
-const todos = [];
+let todos = localStorage.getItem('todo-items')
+  ? JSON.parse(localStorage.getItem('todo-items'))
+  : [];
 
-function createTodo() {
-  const renderItems = function (e) {
+localStorage.setItem('todo-items', JSON.stringify(todos));
+const data = JSON.parse(localStorage.getItem('todo-items'));
+
+function renderData() {
+  // loop twice to sort checked todos to the end: (what about array.sort()?)
+
+  // 1. loop and render unchecked todos
+  data.forEach(todo => {
+    if (todo.checked === true) return;
+    const uncheckedMarkup = `<div class="todo-item">
+        <p>${todo.todoTitle}</p>
+        <button class="check-off">✅</button>
+        <div class="delete">🚫</div>
+       </div>`;
+    todosContainer.insertAdjacentHTML('afterbegin', uncheckedMarkup);
+  });
+
+  // 2. loop and render checked todos
+  data.forEach(todo => {
+    if (todo.checked === false) return;
+    const checkedMarkup = `<div class="todo-item checked">
+        <p>${todo.todoTitle}</p>
+        <button class="check-off">✅</button>
+        <div class="delete">🚫</div>
+       </div>`;
+    todosContainer.insertAdjacentHTML('afterbegin', checkedMarkup);
+  });
+}
+renderData();
+
+function createNewTodo() {
+  const form = document.querySelector('.todo-form');
+  const renderNewTodo = function (e) {
     e.preventDefault();
     const formValue = document.querySelector('.input').value;
 
     // guard clause for blank form entry
     if (formValue === '') return;
 
-    // store state in local storage: ???
+    // push string & todo status to local todos array of objects
+    todos.push({ todoTitle: `${formValue}`, checked: false });
 
-    // push string AND todo status to todos array(object?)
-    todos.push(formValue);
+    // store state (todos array) in local storage
+    if (typeof Storage !== 'undefined') {
+      localStorage.setItem('todo-items', JSON.stringify(todos));
+    } else {
+      // Alert to user that storage is not supported
+      window.alert('Local storage is not supported in your browser.');
+    }
 
-    // clear / reset (prevents rendering duplicates)
+    // clear / reset the page (prevents rendering duplicates)
     document.querySelector('.todos-container').innerHTML = '';
 
-    // loop over todos
-    todos.forEach(todo => {
-      // check todo status: (checked vs unchecked)
+    // render new todo
+    renderData();
 
-      const uncheckedMarkup = `<div class="todo-item ${status}">
-          <p>${todo}</p>
-          <button class="check-off">✅</button>
-          <div class="delete">🚫</div>
-        </div>`;
-      todosContainer.insertAdjacentHTML('afterbegin', uncheckedMarkup);
-    });
-
-    // loop over completeList array to create markup
-    // unchecked.forEach(todo => {
-    //   const uncheckedMarkup = ` <div class="todo-item">
-    //       <p>${todo}</p>
-    //       <button class="check-off">✅</button>
-    //       <div class="delete">🚫</div>
-    //     </div>`;
-    //   todosContainer.insertAdjacentHTML('afterbegin', uncheckedMarkup);
-    // });
-
-    // checked.forEach(todo => {
-    //   const checkedMarkup = ` <div class="todo-item checked">
-    //       <p>${todo}</p>
-    //       <button class="check-off">✅</button>
-    //       <div class="delete">🚫</div>
-    //     </div>`;
-    //   todosContainer.insertAdjacentHTML('afterbegin', checkedMarkup);
-    // });
-
-    // clear form field
+    // clear form field for next entry
     document.querySelector('.todo-form').reset();
   };
 
-  document.querySelector('.todo-form').addEventListener('submit', renderItems);
+  form.addEventListener('submit', renderNewTodo);
 }
-createTodo();
+createNewTodo();
 
-function todoHandler() {
+function handleTodo() {
   const handler = function (e) {
     const todo = e.target.closest('.todo-item');
 
-    // check and uncheck items
+    // 1. check and 2. uncheck items
     if (e.target.classList.contains('check-off')) {
       todo.classList.add('checked');
       todo.style.opacity = '.25';
@@ -74,10 +85,22 @@ function todoHandler() {
 
     // delete items
     if (e.target.classList.contains('delete')) {
-      // build out functionality after createTodo function
+      localStorage.removeItem(`${todo}`);
     }
   };
 
   todosContainer.addEventListener('click', handler);
 }
-todoHandler();
+handleTodo();
+
+function clearAll() {
+  // only show clear btn when there are todo items
+  if (todosContainer.children.length > 1)
+    document.querySelector('.clear-btn').classList.remove('hidden');
+  // hide clear btn when there are no todo items
+  if (todosContainer.children.length === 0)
+    document.querySelector('.clear-btn').classList.add('hidden');
+
+  // localStorage.clear();
+}
+clearAll();
