951767621cfb85c2d50ecd1797f9997d72fe9e60
Billybobchops <chlanda52@gmail.com>
2021-07-01 15:04:20 -0500

    solved submission bug

--- script.js
+++ script.js
@@ -4,12 +4,9 @@ let todos = localStorage.getItem('todo-items')
   ? JSON.parse(localStorage.getItem('todo-items'))
   : [];
 
-// let todosData = JSON.parse(localStorage['todo-items']); // BUG
-// let todosData = JSON.parse(localStorage.getItem('todo-items')); // BUG
-
 const form = document.querySelector('.todo-form');
 const formInput = document.querySelector('.input');
-const formValue = document.querySelector('.input').value;
+// const formValue = document.querySelector('.input').value;
 const todosContainer = document.querySelector('.todos-container');
 const clearBtn = document.querySelector('.clear-btn');
 
@@ -29,7 +26,7 @@ const clearAll = function () {
   toggleClearAllBtn();
 };
 
-function renderData() {
+function renderUI() {
   resetUI();
   let todosClone = [...todos];
   let todosReversed = todosClone.reverse();
@@ -63,6 +60,7 @@ function renderData() {
 function createNewTodo() {
   const renderNewTodo = function (e) {
     e.preventDefault();
+    let formValue = document.querySelector('.input').value;
 
     // guard clause for blank form entry
     if (formValue === '') return;
@@ -78,7 +76,7 @@ function createNewTodo() {
       window.alert('Local storage is not supported in your browser.');
     }
 
-    renderData();
+    renderUI();
     toggleClearAllBtn();
 
     // clear form field for next entry
@@ -90,10 +88,22 @@ function createNewTodo() {
 
 function handleChecks() {
   const checkHandler = function (e) {
+    // if the event target doesn't happen on the todos container or its children, return
+
+    // console.log(e.target.closest('todos-container')); // null
+
+    // if (!e.target.classList.contains('check-off')) return; // can't uncheck items now
+    // if (e.target !== e.target.closest('todos-container')) return; // works but cant check/uncheck etc
+    // if (!e.target.classList.contains('.todo-item')) return; // doesn't work either...
+    // if (!e.target.closest('todos-container')) return;
+    // if (e.target !== todosContainer) return;
+
+    console.log(`click`);
+
+    const todo = e.target.closest('.todo-item');
     function checkTitle(t) {
       return t.todoTitle === todo.firstElementChild.textContent;
     }
-    const todo = e.target.closest('.todo-item');
     const currentTodo = todos.find(checkTitle);
     const i = todos.indexOf(currentTodo);
 
@@ -102,14 +112,14 @@ function handleChecks() {
       // update localStorage
       todos[`${i}`].checked = true; // overwrite
       localStorage['todo-items'] = JSON.stringify(todos); // update
-      renderData();
+      renderUI();
     }
     if (e.target.classList.contains('checked')) {
       todo.classList.remove('checked');
       // update localStorage
       todos[`${i}`].checked = false; // overwrite
       localStorage['todo-items'] = JSON.stringify(todos); // update
-      renderData();
+      renderUI();
     }
   };
 
@@ -119,9 +129,18 @@ function handleChecks() {
 function editTodo() {
   const editHandler = function (e) {
     const todo = e.target.closest('.todo-item');
+
+    // guard clause
+    if (e.target !== todo) return;
+
+    const editTodoText = function () {
+      //
+    };
+
     todo.classList.toggle('todo--active-edit');
 
     if (todo.classList.contains('todo--active-edit')) {
+      // form.addEventListener('submit', editTodotext);
       formInput.focus();
     }
   };
@@ -131,17 +150,19 @@ function editTodo() {
 
 function deleteTodo() {
   const deleteHandler = function (e) {
+    // if (e.target !== e.target.classList.contains('delete')) return; // prevents delete from happening at all...
+
+    const todo = e.target.closest('.todo-item');
     function checkTitle(t) {
       return t.todoTitle === todo.firstElementChild.textContent;
     }
-    const todo = e.target.closest('.todo-item');
     const currentTodo = todos.find(checkTitle);
     const i = todos.indexOf(currentTodo);
 
     if (e.target.classList.contains('delete')) {
       todos.splice(`${i}`, 1);
       localStorage['todo-items'] = JSON.stringify(todos);
-      renderData();
+      renderUI();
     }
   };
 
@@ -149,11 +170,11 @@ function deleteTodo() {
 }
 
 function init() {
-  renderData();
+  renderUI();
   createNewTodo();
   editTodo();
-  deleteTodo();
   handleChecks();
+  deleteTodo();
 }
 
 clearBtn.addEventListener('click', clearAll);
