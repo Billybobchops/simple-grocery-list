8ec022b822d87eefd939b8a9244246bac86bf171
Billybobchops <chlanda52@gmail.com>
2021-07-01 15:43:09 -0500

    refactored code more

--- script.js
+++ script.js
@@ -6,25 +6,12 @@ let todos = localStorage.getItem('todo-items')
 
 const form = document.querySelector('.todo-form');
 const formInput = document.querySelector('.input');
-// const formValue = document.querySelector('.input').value;
 const todosContainer = document.querySelector('.todos-container');
 const clearBtn = document.querySelector('.clear-btn');
 
 function resetUI() {
   todosContainer.innerHTML = '';
 }
-
-function toggleClearAllBtn() {
-  if (todosContainer.children.length > 0) clearBtn.classList.remove('hidden');
-  if (todosContainer.children.length === 0) clearBtn.classList.add('hidden');
-}
-
-const clearAll = function () {
-  resetUI();
-  localStorage.clear();
-  todos = [];
-  toggleClearAllBtn();
-};
 
 function renderUI() {
   resetUI();
@@ -57,6 +44,11 @@ function renderUI() {
   toggleClearAllBtn();
 }
 
+function toggleClearAllBtn() {
+  if (todosContainer.children.length > 0) clearBtn.classList.remove('hidden');
+  if (todosContainer.children.length === 0) clearBtn.classList.add('hidden');
+}
+
 const createNewTodo = function (e) {
   e.preventDefault();
   let formValue = document.querySelector('.input').value;
@@ -82,100 +74,90 @@ const createNewTodo = function (e) {
   form.reset();
 };
 
-function handleChecks() {
-  const checkHandler = function (e) {
-    // if the event target doesn't happen on the todos container or its children, return
+const handleChecks = function (e) {
+  // if the event target doesn't happen on the todos container or its children, return
 
-    // console.log(e.target.closest('todos-container')); // null
+  // console.log(e.target.closest('todos-container')); // null
 
-    // if (!e.target.classList.contains('check-off')) return; // can't uncheck items now
-    // if (e.target !== e.target.closest('todos-container')) return; // works but cant check/uncheck etc
-    // if (!e.target.classList.contains('.todo-item')) return; // doesn't work either...
-    // if (!e.target.closest('todos-container')) return;
-    // if (e.target !== todosContainer) return;
+  // if (!e.target.classList.contains('check-off')) return; // can't uncheck items now
+  // if (e.target !== e.target.closest('todos-container')) return; // works but cant check/uncheck etc
+  // if (!e.target.classList.contains('.todo-item')) return; // doesn't work either...
+  // if (!e.target.closest('todos-container')) return;
+  // if (e.target !== todosContainer) return;
 
-    console.log(`handleChecks() function click`);
+  console.log(`handleChecks() function click`);
 
-    const todo = e.target.closest('.todo-item');
-    function checkTitle(t) {
-      return t.todoTitle === todo.firstElementChild.textContent;
-    }
-    const currentTodo = todos.find(checkTitle);
-    const i = todos.indexOf(currentTodo);
+  const todo = e.target.closest('.todo-item');
+  function checkTitle(t) {
+    return t.todoTitle === todo.firstElementChild.textContent;
+  }
+  const currentTodo = todos.find(checkTitle);
+  const i = todos.indexOf(currentTodo);
 
-    if (e.target.classList.contains('check-off')) {
-      todo.classList.add('checked');
-      // update localStorage
-      todos[`${i}`].checked = true; // overwrite
-      localStorage['todo-items'] = JSON.stringify(todos); // update
-      renderUI();
-    }
-    if (e.target.classList.contains('checked')) {
-      todo.classList.remove('checked');
-      // update localStorage
-      todos[`${i}`].checked = false; // overwrite
-      localStorage['todo-items'] = JSON.stringify(todos); // update
-      renderUI();
-    }
+  if (e.target.classList.contains('check-off')) {
+    todo.classList.add('checked');
+    // update localStorage
+    todos[`${i}`].checked = true; // overwrite
+    localStorage['todo-items'] = JSON.stringify(todos); // update
+    renderUI();
+  }
+  if (e.target.classList.contains('checked')) {
+    todo.classList.remove('checked');
+    // update localStorage
+    todos[`${i}`].checked = false; // overwrite
+    localStorage['todo-items'] = JSON.stringify(todos); // update
+    renderUI();
+  }
+};
+
+const updateTodo = function (e) {
+  const todo = e.target.closest('.todo-item');
+
+  // guard clause
+  // if (e.target !== todo) return; // doesn't work
+
+  const editTodoText = function () {
+    //
   };
 
-  todosContainer.addEventListener('click', checkHandler);
-}
+  todo.classList.toggle('todo--active-edit');
 
-function editTodo() {
-  const editHandler = function (e) {
-    const todo = e.target.closest('.todo-item');
+  if (todo.classList.contains('todo--active-edit')) {
+    form.addEventListener('submit', editTodoText);
+    formInput.focus();
+    // createNewTodo(); // Cannot read property 'preventDefault' of undefined
+    form.removeEventListener('submit', editTodoText);
+  }
+};
 
-    // guard clause
-    // if (e.target !== todo) return; // doesn't work
+const deleteTodo = function (e) {
+  // if (e.target !== e.target.classList.contains('delete')) return; // prevents delete from happening at all...
 
-    const editTodoText = function () {
-      //
-    };
+  const todo = e.target.closest('.todo-item');
+  function checkTitle(t) {
+    return t.todoTitle === todo.firstElementChild.textContent;
+  }
+  const currentTodo = todos.find(checkTitle);
+  const i = todos.indexOf(currentTodo);
 
-    todo.classList.toggle('todo--active-edit');
+  if (e.target.classList.contains('delete')) {
+    todos.splice(`${i}`, 1);
+    localStorage['todo-items'] = JSON.stringify(todos);
+    renderUI();
+  }
+};
 
-    if (todo.classList.contains('todo--active-edit')) {
-      form.addEventListener('submit', editTodoText);
-      formInput.focus();
-      //
-      form.removeEventListener('submit', editTodoText);
-    }
-  };
+const clearAll = function () {
+  resetUI();
+  localStorage.clear();
+  todos = [];
+  toggleClearAllBtn();
+};
 
-  todosContainer.addEventListener('click', editHandler);
-}
-
-function deleteTodo() {
-  const deleteHandler = function (e) {
-    // if (e.target !== e.target.classList.contains('delete')) return; // prevents delete from happening at all...
-
-    const todo = e.target.closest('.todo-item');
-    function checkTitle(t) {
-      return t.todoTitle === todo.firstElementChild.textContent;
-    }
-    const currentTodo = todos.find(checkTitle);
-    const i = todos.indexOf(currentTodo);
-
-    if (e.target.classList.contains('delete')) {
-      todos.splice(`${i}`, 1);
-      localStorage['todo-items'] = JSON.stringify(todos);
-      renderUI();
-    }
-  };
-
-  todosContainer.addEventListener('click', deleteHandler);
-}
-
-function init() {
-  renderUI();
-  createNewTodo();
-  editTodo();
-  handleChecks();
-  deleteTodo();
-}
+renderUI();
 
 form.addEventListener('submit', createNewTodo);
+todosContainer.addEventListener('click', handleChecks);
+todosContainer.addEventListener('click', updateTodo);
+todosContainer.addEventListener('click', deleteTodo);
 clearBtn.addEventListener('click', clearAll);
-
-init();
